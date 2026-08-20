f85591bc408a9ca94e159fe98eff7129a6c9ebb6
jjbastoh <145081165+jjbastoh@users.noreply.github.com>
2026-08-20 13:45:51 -0500

    Update interfaces.py

--- interfaces.py
+++ interfaces.py
@@ -9,29 +9,36 @@ st.write("fácilmente etc")
 image = Image.open("images.jpg")
 st.image(image, caption="interfaces multi")
 
-texto = st.text_input('el texto, es')
-st.write('este coso?,texto')
+texto = st.text_input("el texto, es")
+st.write("este coso?, texto")
 
 
 st.subheader("ahora 2 col")
 col1, col2 = st.columns(2)
 
 with col1:
-  st.subheader("esta es la col 1")
-  st.write("las checkbox")
-  resp = st.checkbox('estoy de acuerdo')
-  if resp:
-    st.write('correcto')
+    st.subheader("esta es la col 1")
+    st.write("las checkbox")
+
+    resp = st.checkbox("estoy de acuerdo")
+
+    if resp:
+        st.write("correcto")
+
 
 with col2:
-  st.subheader("esta es la 2 col")
-  modo = st.radio("que modalidad es la principal , ('visual' , 'audi' ,'lol'))if
-  if modo == 'visual':
-      st.write("la vista es fundamental para tu interfaz")
+    st.subheader("esta es la 2 col")
 
-  if modo == "auditiva":
-    st.write("la audicion es fundamental para tu interfaz")
+    modo = st.radio(
+        "¿Qué modalidad es la principal?",
+        ("visual", "auditiva", "táctil")
+    )
 
-  if modo == "tactil":
-    st.write("el tactil es fundamenta para tu interfaz")
-    
+    if modo == "visual":
+        st.write("La vista es fundamental para tu interfaz")
+
+    if modo == "auditiva":
+        st.write("La audición es fundamental para tu interfaz")
+
+    if modo == "táctil":
+        st.write("El táctil es fundamental para tu interfaz")

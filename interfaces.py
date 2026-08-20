ae558f9581bc7eeb3bb64d04e42a8b17f97effb7
jjbastoh <145081165+jjbastoh@users.noreply.github.com>
2026-08-20 13:19:13 -0500

    Update interfaces.py

--- interfaces.py
+++ interfaces.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.title("hola mundo")
+from PIL import Image
+st.title("si")
+
+st.header("en este espacio comienxo etc")
+st.write("facilmente etc")
+image = Image.open('images.png')
+st.image(image, caption ='interfaces multi')
